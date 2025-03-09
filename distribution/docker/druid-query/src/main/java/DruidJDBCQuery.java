c20cc0415521dc27bef1c9537b53f70d55c284df
Andrew Ho <a.ho@salesforce.com>
2025-03-09 15:40:06 -0700

    Add class for executing queries using JDBC

---
+++ distribution/docker/druid-query/src/main/java/DruidJDBCQuery.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DruidJDBCQuery
+{
+  private static final String DRUID_JDBC_URL = "jdbc:avatica:remote:url=http://localhost:8888/druid/v2/sql/avatica/";
+  private static final String QUERY = "SELECT * FROM events_sink LIMIT 1";
+  public static void main(String[] args) throws Exception
+  {
+    Class.forName("org.apache.calcite.avatica.remote.Driver");
+    try (
+        Connection connection = DriverManager.getConnection(DRUID_JDBC_URL);
+        Statement statement = connection.createStatement();
+        final ResultSet rs = statement.executeQuery(QUERY);
+    ) {
+        StringBuilder builder = new StringBuilder();
+        while (rs.next()) {
+          for (int ordinal = 1; ordinal <= rs.getMetaData().getColumnCount(); ordinal++) {
+            String columnName = rs.getMetaData().getColumnName(ordinal);
+            String columnValue = rs.getObject(ordinal).toString();
+            builder.append(columnName);
+            builder.append("=");
+            builder.append(columnValue);
+            builder.append(";");
+          }
+        }
+        System.out.println(builder);
+    }
+  }
+}
