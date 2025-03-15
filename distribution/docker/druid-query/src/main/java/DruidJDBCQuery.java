df13ad8822c49a1b2247b2a49641cdce01150209
Andrew Ho <a.ho@salesforce.com>
2025-03-14 17:12:11 -0700

    Handle null value in JDBC query

--- distribution/docker/druid-query/src/main/java/DruidJDBCQuery.java
+++ distribution/docker/druid-query/src/main/java/DruidJDBCQuery.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 public class DruidJDBCQuery
 {
   private static final String DRUID_JDBC_URL = "jdbc:avatica:remote:url=http://localhost:8888/druid/v2/sql/avatica/";
-  private static final String QUERY = "SELECT * FROM events_sink LIMIT 1";
+  private static final String QUERY = "SELECT * FROM events_sink ORDER BY __time DESC LIMIT 1";
   public static void main(String[] args) throws Exception
   {
     Class.forName("org.apache.calcite.avatica.remote.Driver");
@@ -19,7 +19,10 @@ public class DruidJDBCQuery
         while (rs.next()) {
           for (int ordinal = 1; ordinal <= rs.getMetaData().getColumnCount(); ordinal++) {
             String columnName = rs.getMetaData().getColumnName(ordinal);
-            String columnValue = rs.getObject(ordinal).toString();
+            String columnValue = "";
+            if (rs.getObject(ordinal) != null) {
+              columnValue = rs.getObject(ordinal).toString();
+            }
             builder.append(columnName);
             builder.append("=");
             builder.append(columnValue);
